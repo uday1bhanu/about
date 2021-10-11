53007bde309822f8b9e249043c54ff7cc6951bc8
Uday Bhanu Prasad Kanagala <uday.kanagala@anaplan.com>
2021-10-11 16:51:42 -0700

    add envs

--- src/main/java/com/anaplan/about/controller/AboutController.java
+++ src/main/java/com/anaplan/about/controller/AboutController.java
@@ -5,11 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class AboutController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static Map<String, String> env;
+
+    static {
+        env = System.getenv();
+    }
 
     /**
      * Get about message
@@ -18,7 +24,12 @@ public class AboutController {
      */
     @RequestMapping(value = "/about", method = RequestMethod.GET)
     public String about(){
-        logger.info("I'am sample app v5");
-        return "I'am sample app v5";
+        String dataCenter = env.get("DC_NAME");
+        String cluster = env.get("CLUSTER_NAME");
+        String verion = env.get("VERSION");
+        String responseOut = dataCenter + "-" + cluster + "-"+verion + "-"+"I'am sample app";
+
+        logger.info(responseOut);
+        return responseOut;
     }
 }
